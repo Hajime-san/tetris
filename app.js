56dc911dd3a97e89dd00498e102bedf7c71ea58b
Hajime-san <utd.c.r.d.s.a@gmail.com>
2019-09-29 18:48:12 +0900

    [update] rotatable

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas').getContext('2d');
 let controllableBlock = 0;
 let nowBlocks = [];
 let howManyFall = 0;
 let left = true;
 let right = true;
 let down = true;
+let angle = 0;
 let stop = false;
 let isComplete = false;
-const ROW = 9;
+const ROW = 10;
 const COLUMN = 14;
 
 /**
@@ -18,69 +18,77 @@ const COLUMN = 14;
  * @param {number} 2 - fixed block area
  */
 let field = [
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0,
-  // 0,0,0,0,0,0,0,0,0,0
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty',1,'empty','empty',
+  'empty','empty','empty','empty','empty','empty','empty',1,'empty','empty',
+  0,0,'empty','empty','empty','empty','empty',1,0,0,
+  0,0,1,1,1,1,'empty',1,0,0
 ];
-for(let i = 0; i < COLUMN*(ROW+1); i++){
-  field.push(false)
-}
+//let field = [...Array(COLUMN*ROW)].forEach(()=> field = false);
 
 const a = 4;
 
 const BLOCKS = [
   {
-    number: [a,a+1,a+ROW+1,a+ROW+2],
+    number: [a,a+1,a+ROW,a+ROW+1],
     color: 'red',
   },
   {
-    number: [a,a+1,a+2,a+3],
+    number: [a,a+ROW,a+(ROW*2),a+(ROW*3)],
     color: 'blue',
   },
   {
-    number: [a,a+1,a+2,a+ROW+2],
+    number: [a,a+1,a+ROW,a+(ROW*2)],
     color: 'green',
   },
   {
-    number: [a,a+1,a+2,a+ROW+1],
-    color: 'yellow',
-  },
-  {
-    number: [a,a+1,a+2,a+ROW+3],
+    number: [a,a+1,a+ROW+1,a+(ROW*2)+1],
     color: 'orange',
   },
   {
-    number: [a,a+1,a+ROW,a+ROW+1],
+    number: [a,a+ROW,a+ROW+1,a+(ROW*2)],
+    color: 'yellow',
+  },
+  {
+    number: [a,a+ROW,a+ROW+1,a+(ROW*2)+1],
     color: 'purple',
   },
   {
-    number: [a-1,a,a+ROW+1,a+ROW+2],
-    color: 'gray',
+    number: [a+1,a+ROW,a+ROW+1,a+(ROW*2)],
+    color: 'pink',
   },
 ];
 
-let copyBLOCKS = JSON.parse(JSON.stringify(BLOCKS))
+let copyBLOCKS = JSON.parse(JSON.stringify(BLOCKS));
+
 
 function createNewBlock() {
-  return controllableBlock = Math.floor( Math.random() * BLOCKS.length );
-  //return controllableBlock = 1;
+  controllableBlock = 5;
+  //return controllableBlock = Math.floor( Math.random() * BLOCKS.length );
 }
 
-function updateBlocks(parts,count) {
+function updateBlocks(parts,direction) {
+  const dir = (
+    (direction === 'down') ? ROW
+  : (direction === 'left') ? -1
+  : (direction === 'right') ? 1
+  : null );
   for(let i = 0; i < parts.length; i++){
-    parts[i] += count;
+    parts[i] += dir;
   }
+}
+
+function fixToFirstDigit(number) {
+  return number.toFixed().substr(-1, 1);
 }
 
 function resetBlocks() {
@@ -88,250 +96,428 @@ function resetBlocks() {
 }
 
 
-function updateField() {
-  copyBLOCKS[controllableBlock].number.forEach((e,i) => {
-    field[e] = true;
+function updateField(array) {
+  array.forEach((v) => {
+    field[v] = 'current';
   });
 }
 
 function clearField() {
-  nowBlocks.forEach((e,i) => {
-    field[e] = false;
+  nowBlocks.forEach((v) => {
+    field[v] = 'empty';
   });
 }
 
 function controllableBlocksNumber() {
-    nowBlocks.length = 0;
-    field.forEach((e,i) => {
-      if (e === true) {
-        nowBlocks = [...nowBlocks,i]
-      }
-    });
+  nowBlocks.length = 0;
+  field.forEach((v,i) => {
+    if (v === 'current') {
+      nowBlocks = [...nowBlocks,i];
+    }
+  });
 }
 function clearCanvas() {
-  ctx.clearRect(0, 0, 300, 420);
+  canvas.clearRect(0, 0, 300, 420);
 }
 
 function blockMovable() {
-  return new Promise((resolve)=>{
-    left = true;
-    right = true;
-    down = true;
+  left = true;
+  right = true;
+  down = true;
 
-    // check movable around block
-    field.forEach((e,i) => {
-      if(e !== false && e !== true) {
-        copyBLOCKS[controllableBlock].number.forEach((d,j) => {
-          if(copyBLOCKS[controllableBlock].number[j] === (i+1)) {
-            console.log('left failed');
-            left = false;
-          } else if(copyBLOCKS[controllableBlock].number[j] === (i-1)) {
-            console.log('right failed');
-            right = false;
-          } else if(field[i-(ROW+1)] === true) {
-            console.log('down failed');
-            down = false;
-          }
-        })
-      }
-    });
-    
-    
-    copyBLOCKS[controllableBlock].number.forEach((e,i) => {
-      // check last row
-      const isLastRow = field.some(v => e >= field.length - (ROW+1));
-      if(isLastRow) {
-        console.log('last row');
-        down = false;
-      }
-
-      // check left/right walls if its number of the first digit is 0(left)/9(right)
-      if(copyBLOCKS[controllableBlock].number[i].toFixed().substr(-1, 1) === (ROW+1).toFixed().substr(-1, 1)) {
-        console.log('left walled');
+  // check movable around block
+  field.forEach((v,i) => {
+    if(typeof v !== "number") {
+      return;
+    }
+    copyBLOCKS[controllableBlock].number.forEach((_,j,arr) => {
+      if(arr[j] === (i+1)) {
+        console.log('left failed');
         left = false;
-      } else if(copyBLOCKS[controllableBlock].number[i].toFixed().substr(-1, 1) === ROW.toFixed()) {
-        console.log('right walled');
+      }
+      if(arr[j] === (i-1)) {
+        console.log('right failed');
         right = false;
       }
+      if(field[i-ROW] === true) {
+        console.log('down failed');
+        down = false;
+      }
     })
-    resolve()
+  })
+  
+  
+  
+  copyBLOCKS[controllableBlock].number.forEach((v,i,arr) => {
+    // check last row
+    const isLastRow = field.some(some => v >= field.length - ROW);
+    if(isLastRow) {
+      console.log('last row');
+      down = false;
+    }
+
+    // check left/right walls if its number of the first digit is 0(left)/9(right)
+    if(fixToFirstDigit(arr[i]) === fixToFirstDigit(ROW)) {
+      console.log('left walled');
+      left = false;
+    }
+    if(fixToFirstDigit(arr[i]) === fixToFirstDigit(ROW-1)) {
+      console.log('right walled');
+      right = false;
+    }
   })
   
 }
 
+function rotateAngle() {
+  angle += 90
+  if(angle === 360) {
+    angle = 0;
+  }
+}
+
+
+function translateNumberToRect(num,o) {
+  if(num <= o && (o - num) <= 2 ) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), 0];
+  }
+  if (num >= o && (num - o) <= 2) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), 0];
+  }
+  if (num <= o && (o - num) >= 3 && (o - num) <= 13) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), 1];
+  }
+  if (num >= o && (num - o) >= 3 && (num - o) <= 13) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), -1];
+  }
+  if (num <= o && (o - num) >= 3 && (o - num) >= 13) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), 2];
+  }
+  if (num >= o && (num - o) >= 3 && (num - o) >= 13) {
+    return [fixToFirstDigit(num) - fixToFirstDigit(o), -2];
+  }
+}
+
+function rotate(rect) {
+  const radians = (Math.PI / 180) * 90,
+      cos = Math.cos(radians),
+      sin = Math.sin(radians),
+      nx = (cos * (rect[0] - 0)) + (sin * (rect[1] - 0));
+      ny = (cos * (rect[1] - 0)) - (sin * (rect[0] - 0));
+  return [Math.round(nx), Math.round(ny)];
+}
+
+function translateRectToNum(rotateRect) {
+  if(rotateRect[0] === 0 && rotateRect[1] > 0 ){
+    return -(rotateRect[1]*ROW);
+  }
+  if(rotateRect[0] === 0 && rotateRect[1] < 0 ){
+    return -(rotateRect[1]*ROW);
+  }
+  if(rotateRect[0] === 0 && rotateRect[1] > 0 ){
+    return (rotateRect[1]*ROW);
+  }
+  if(rotateRect[0] > 0 && rotateRect[1] === 0 ){
+    return rotateRect[0];
+  }
+  if(rotateRect[0] < 0 && rotateRect[1] === 0 ){
+    return rotateRect[0];
+  }
+  if(rotateRect[0] > 0 && rotateRect[1] > 0 ){
+    return -(rotateRect[1]*ROW)+rotateRect[0]
+  }
+  if(rotateRect[0] > 0 && rotateRect[1] < 0 ){
+    return -(rotateRect[1]*ROW)+rotateRect[0]
+  }
+  if(rotateRect[0] < 0 && rotateRect[1] > 0 ){
+    return -(rotateRect[1]*ROW)+rotateRect[0]
+  }
+  if(rotateRect[0] < 0 && rotateRect[1] < 0 ){
+    return -(rotateRect[1]*ROW)+rotateRect[0]
+  }
+  if(rotateRect[0] === 0 && rotateRect[1] === 0 ){
+    return 0;
+  }
+}
+
+
+
+function blockRotatableArray() {
+  // position of organization point
+  let center;
+  // fix position after rotated
+  let fixPosition;
+  
+  // cant rotate when it's square
+  if(controllableBlock === 0) {
+    return;
+  }
+
+  // bar block
+  if(controllableBlock === 1){
+    center = 1;
+  }
+  if(controllableBlock === 1 && angle === 0){
+    fixPosition = 0;
+  } else if(controllableBlock === 1 && angle === 270){
+    fixPosition = 0;
+  } else {
+    fixPosition = 1;
+  }
+
+  // reverse L block
+  if(controllableBlock === 2){
+    fixPosition = 0;
+  }
+  if(controllableBlock === 2 && angle === 0){
+    center = 2;
+  } else if (controllableBlock === 2 && angle === 90) {
+    center = 2;
+  } else {
+    center = 1;
+  }
+
+  // L block
+  if(controllableBlock === 3){
+    fixPosition = 0;
+  }
+  if(controllableBlock === 3 && angle === 0){
+    center = 1;
+  } else if (controllableBlock === 3 && angle === 90) {
+    center = 2;
+  } else if (controllableBlock === 3 && angle === 180) {
+    center = 2;
+  } else if (controllableBlock === 3 && angle === 270) {
+    center = 1;
+  }
+
+  // T block
+  if(controllableBlock === 4){
+    fixPosition = 0;
+  }
+  if(controllableBlock === 4 && angle === 0){
+    center = 0;
+  } else if (controllableBlock === 4 && angle === 90) {
+    center = 2;
+  } else if (controllableBlock === 4 && angle === 180) {
+    center = 3;
+  } else if (controllableBlock === 4 && angle === 270) {
+    center = 1;
+  }
+
+  // _| block
+  if(controllableBlock === 5){
+    fixPosition = 0;
+  }
+  if(controllableBlock === 5 && angle === 0){
+    center = 3;
+    fixPosition = -(ROW+1);
+  } else if (controllableBlock === 5 && angle === 90) {
+    center = 2;
+  } else if (controllableBlock === 5 && angle === 180) {
+    center = 3;
+  } else if (controllableBlock === 5 && angle === 270) {
+    center = 2;
+    fixPosition = ROW-1;
+  }
+
+  // |_ block
+  if(controllableBlock === 6){
+    center = 2;
+    fixPosition = 0;
+  }
+  if (controllableBlock === 6 && angle === 0) {
+    fixPosition = -(ROW+1);
+  }
+  if (controllableBlock === 6 && angle === 270) {
+    fixPosition = ROW-1;
+  }
+  
+  
+  const rotateBlocks = nowBlocks.map((v)=>{
+            num = translateNumberToRect(v,nowBlocks[center])
+            rect = rotate(num)
+            update = translateRectToNum(rect)
+    return update+nowBlocks[center]+fixPosition;
+
+  })
+  return rotateBlocks;
+}
+
+
+
 
 function isBlocksGathersInRow() {
-  return new Promise((resolve)=>{
-    if(left === false && right === false && down === false) {
-      isComplete = true;
-  
-      let oneRowArray = []; // creat Array on each rows
-      let start = 0;
-      let end = ROW+1;
-      for(let i = 0; i < COLUMN; i++){
-        let oneROW = field.slice(start,end);
-        //oneRowArray.push(oneROW);
-        oneRowArray = [...oneRowArray,oneRow];
-        start += ROW+1;
-        end += ROW+1;
+  if(left === false && right === false && down === false) {
+    isComplete = true;
+
+    let start = 0;
+    let end = ROW;
+    const oneRowArray = [...Array(COLUMN)].map(()=>{
+      oneROW = field.slice(start,end);
+      start += ROW;
+      end += ROW;
+      return oneROW;
+    })
+
+    let completeRowArray = []; // should delete areas
+    let completeRowNumbers = []; // row numbers
+    oneRowArray.forEach((v,i)=>{
+      // skip if the row include 0
+      const isIncludeZero = v.every(item => item !== 'empty');
+      if(isIncludeZero) {
+        
+        completeRowNumbers.push(i);
+
+        [...Array(ROW)].forEach((_,j)=>{
+          completeRowArray.push((i*ROW)+j);
+        })
       }
-  
-      let completeRowArray = []; // should delete areas
-      let completeRowNumbers = []; // row numbers
-      oneRowArray.forEach((e,i)=>{
-        // skip if the row include 0
-        const isIncludeZero = e.every(item => item !== 0);
-        if(isIncludeZero) {
-          completeRowNumbers.push(i)
-          for(let j = 0; j < ROW+1; j++){
-            completeRowArray.push(i*(ROW+1)+j);
-          }
-        }
-      })
-      
-  
-      // delete complete rows
-      completeRowArray.forEach((e,i)=>{ field[e] = 0; })
-      clearCanvas();
-      draw();
-  
-      // move blocks as the amount of deleted rows
-      const lowerBlocks = field.map((e,i)=>{
-        if (i <= completeRowNumbers[0]*(ROW+1)) {
-          if(e === 1 || e === 2) {
-            field[i] = 0;
-            return i+((completeRowNumbers.length)*(ROW+1));
-          }
-        }
-      })
-      lowerBlocks.forEach((e,i) => {
-        field[e] = 2;
-      });
-  
-      setTimeout(clearCanvas, 1500);
-      setTimeout(draw, 1500);
-  
-      
-      // let c = [95,103,104,105];
-      // let ee = [];
-      // field.forEach((e,i) => {
-      //   if(e === 0) {
-      //     c.forEach((d,j) => {
-      //       if(field[i-(ROW+1)] === 2) {
-      //         field[d] = 0;
-      //         ee.push(d+(ROW+1));
-      //       }
-      //     })
-      //   }
-      // })
-      
-      // ee.forEach((e,i) => {
-      //   field[e] = 2;
-      // });
-      //stop = true;
-      
-      resolve();
-      
-      
-      // setTimeout(clearCanvas, 1500);
-      // setTimeout(draw, 1500);
-      
-  
-    }
-  })
+    })
+
+    // delete complete rows
+    completeRowArray.forEach((v)=>{ field[v] = 'empty'; })
+    clearCanvas();
+    draw();
+
+    // move blocks as the amount of deleted rows
+    const lowerBlocks = field.map((v,i,arr)=>{
+      if (i >= completeRowNumbers[0]*ROW) {
+        return;
+      }
+      if(v !== 'empty') {
+        arr[i] = 'empty';
+        return [v,i+(completeRowNumbers.length*ROW)];
+      }
+    }).filter(f => f);
+    
+    lowerBlocks.forEach((v) => {
+      field[v[1]] = v[0]
+    });
+    
+    setTimeout(clearCanvas, 1500);
+    setTimeout(draw, 1500);
+
+    
+    // let c = [95,103,104,105];
+    // let ee = [];
+    // field.forEach((e,i) => {
+    //   if(e === 0) {
+    //     c.forEach((d,j) => {
+    //       if(field[i-(ROW+1)] === 2) {
+    //         field[d] = 0;
+    //         ee.push(d+(ROW+1));
+    //       }
+    //     })
+    //   }
+    // })
+    
+    // ee.forEach((e,i) => {
+    //   field[e] = 2;
+    // });
+    //stop = true;
+
+  }
 }
 
 
 function draw() {
-  if (canvas.getContext) {
-    let outline = new Path2D();
-    outline.rect(0, 0, 300, 420);
-    ctx.stroke(outline);
-    
-    field.forEach((e,i)=>{
-      if(e !== false && e === true) {
-        ctx.fillStyle =copyBLOCKS[controllableBlock].color;
-        ctx.fillRect((i.toFixed().substr(-1, 1))*30, Math.floor(i/10)*30, 30, 30);
-        ctx.fill();
-      } else if (e !== false && e !== true) {
-        ctx.fillStyle =copyBLOCKS[e].color;
-        ctx.fillRect((i.toFixed().substr(-1, 1))*30, Math.floor(i/10)*30, 30, 30);
-        ctx.fill();
-      }
-      // } else if (e === 2) {
-      //   ctx.fillStyle =　'black';
-      //   ctx.fillRect((i.toFixed().substr(-1, 1))*30, Math.floor(i/10)*30, 30, 30);
-      //   ctx.fill();
-      // } 
-    })
-    
+  if (!canvas) {
+    return;
   }
+  let outline = new Path2D();
+  outline.rect(0, 0, 300, 420);
+  canvas.stroke(outline);
+  
+  field.forEach((v,i)=>{
+    // draw controllable block
+    if(v !== 'empty' && v === 'current') {
+      canvas.fillStyle = copyBLOCKS[controllableBlock].color;
+      canvas.fillRect(fixToFirstDigit(i)*30, Math.floor(i/10)*30, 30, 30);
+      canvas.fill();
+    // draw fixed block
+    }
+    if (v !== 'empty' && v !== 'current') {
+      canvas.fillStyle = copyBLOCKS[v].color;
+      canvas.fillRect(fixToFirstDigit(i)*30, Math.floor(i/10)*30, 30, 30);
+      canvas.fill();
+    }
+  })
 }
 
 window.addEventListener('keydown', event => {
   // down direction
   if (event.isComposing || event.keyCode === 40 && down) {   
     console.log('down')
-    updateBlocks(copyBLOCKS[controllableBlock].number,10);
+    updateBlocks(copyBLOCKS[controllableBlock].number,'down');
     clearField();
     clearCanvas();
-    updateField();
+    updateField(copyBLOCKS[controllableBlock].number);
     draw();
-    blockMovable();
-    //isBlocksGathersInRow();
+
   // left direction
   } else if (event.isComposing || event.keyCode === 37 && left) {
     console.log('left')
-    updateBlocks(copyBLOCKS[controllableBlock].number,-1);
+    updateBlocks(copyBLOCKS[controllableBlock].number,'left');
     clearField();
     clearCanvas();
-    updateField();
+    updateField(copyBLOCKS[controllableBlock].number);
     draw();
-    blockMovable();
-    //isBlocksGathersInRow();
+
   // right direction
   } else if (event.isComposing || event.keyCode === 39 && right) {
     console.log('right');
-    updateBlocks(copyBLOCKS[controllableBlock].number,1);
+    updateBlocks(copyBLOCKS[controllableBlock].number,'right');
     clearField();
     clearCanvas();
-    updateField();
+    updateField(copyBLOCKS[controllableBlock].number);
     draw();
-    blockMovable();
-    //isBlocksGathersInRow();
+    
+  } else if (event.isComposing || event.keyCode === 38 && rotate) {
+    console.log('rotate');
+    rotateAngle();
+    console.log(angle);
+    clearField();
+    clearCanvas();
+    updateField(blockRotatableArray());
+    draw();
+    
   }
+
+  
+  blockMovable();
+  
   controllableBlocksNumber();
+  
   if(down === false) {
-    console.log('CREATE!!!!!');
-    console.log(controllableBlock);
     resetBlocks();
-    field.forEach((e,i)=>{
-      if(e === true) {
+    field.forEach((v,i)=>{
+      if(v === 'current') {
         field[i] = controllableBlock;
       }
     })
-    controllableBlocksNumber();
-    clearField();
-    clearField();
-    clearCanvas();
-    createNewBlock();
-    updateField();
-    controllableBlocksNumber();
-    draw();
-    down = true;
-    console.log(field);
-    console.log(BLOCKS[1].number);
+  //   controllableBlocksNumber();
+  //   clearField();
+  //   clearCanvas();
+  //   createNewBlock();
+  //   updateField();
+  //   controllableBlocksNumber();
+  //   draw();
+  //   down = true;
   }
 
+  isBlocksGathersInRow();
+  console.log(field);
+  console.log(nowBlocks);
   
 });
 
 
 window.addEventListener('load',()=>{
   createNewBlock();
-  updateField();
+  updateField(copyBLOCKS[controllableBlock].number);
   draw();
-  blockMovable();
   controllableBlocksNumber();
+  blockMovable();
+  console.log(field);
 })
-
